57b69675202d178cd59469353d313f2fbff4247d
bilalnawaz234 <bilalnawaz234@gmail.com>
2026-01-17 18:11:57 -0500

    Create Product.java
    Add Product model for business data representation

---
+++ src/main/java/com/analytics/model/Product.java
@@ -0,0 +1,34 @@
+package com.analytics.model;
+
+public class Product {
+
+    private String name;
+    private double price;
+    private int unitsSold;
+    private double variableCost;
+    private double fixedCost;
+
+    public Product(String name, double price, int unitsSold, double variableCost, double fixedCost) {
+        this.name = name;
+        this.price = price;
+        this.unitsSold = unitsSold;
+        this.variableCost = variableCost;
+        this.fixedCost = fixedCost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getRevenue() {
+        return price * unitsSold;
+    }
+
+    public double getTotalCost() {
+        return (variableCost * unitsSold) + fixedCost;
+    }
+
+    public double getProfit() {
+        return getRevenue() - getTotalCost();
+    }
+}
